368e4609d49e96ec324c02d45805027f638a72b7
mohitmishra12 <mohitmishra747041@gmail.com>
2025-07-08 11:54:57 +0530

    first commit

---
+++ src/main/java/com/example/loging/DemoLogingApplication.java
@@ -0,0 +1,13 @@
+package com.example.loging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLogingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLogingApplication.class, args);
+	}
+
+}
