fff7b6a06f69c4f187cd1dbd1ed6c36901a63d90
mohitmishra12 <mohitmishra747041@gmail.com>
2025-07-08 15:51:57 +0530

    logger frist commit

--- src/main/java/com/example/loging/homecontroller/controller.java
+++ src/main/java/com/example/loging/homecontroller/controller.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
@@ -16,10 +17,14 @@ public class controller {
 private Logger logger = LoggerFactory.getLogger(controller.class);
 
     @GetMapping
-    public String Home(){
+    public String Home(
+            @RequestParam(defaultValue = "defailt value ",required = false) String value
+    ){
+
+
 //        System.out.println("home controller");
-        logger.info("This is info calling");
-        logger.warn("This is warn calling");
+        logger.info("This is info calling {}",value);
+        logger.warn("This is warn calling{}",value);
         logger.trace("This is trace calling");
         logger.error("This is error calling");
         logger.debug("This is debug calling");
